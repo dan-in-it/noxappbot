7988860e02f15e1455951a87fe122cad34aa1bcb
dan-in-it <105667614+dan-in-it@users.noreply.github.com>
2025-10-04 19:41:13 -0700

    Add weekday raid teams to application question

--- src/bot.py
+++ src/bot.py
@@ -36,7 +36,7 @@ intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 questions = [
-    "Which raid team are you applying to? Weekend (Fri/Sat), Floater/Casual",
+    "Which raid team are you applying to? Weekend (Fri/Sat), Floater/Casual, 10M Weasals Weekday Team, 10M Casual Weekday Team",
     "Have you reviewed the raid schedule for the team you're applying for?",
     "How did you hear about us?",
     "Is there a certain class/spec/role that you prefer to play?",
